8b70c2ff2a945b9920037b0ee4ba640eded48d5b
Erik Huang <ekhu94@gmail.com>
2021-04-26 11:51:13 -0700

    adds express.urlencoded parser built into express and post request for submitting a product

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-const http = require("http");
-
 const express = require("express");
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   console.log("In the first middleware");
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("In the second middleware");
+app.use("/add-product", (req, res) => {
+  res.send(
+    '<form method="POST" action="/product"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
 });
 
-const server = http.createServer(app);
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
-server.listen(3000);
+app.use("/", (req, res, next) => {
+  console.log("In the second middleware");
+  res.send(
+    '<h1>Welcome to my Express App</h1><a href="/add-product">New Product</a>'
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Server listening for requests on Port 3000");
+});
