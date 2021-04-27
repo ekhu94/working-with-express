5e96d39508b47076314ea1461b8313c716bf0e0e
Erik Huang <ekhu94@gmail.com>
2021-04-27 05:31:05 -0700

    adds 404 html page route

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res.send("<h1>404: Page Not Found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000, () => {
