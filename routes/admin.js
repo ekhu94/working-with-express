f5fbc7fd8927e34b30a50566c0f78b9d4d80e8ab
Erik Huang <ekhu94@gmail.com>
2021-04-27 05:40:48 -0700

    adds utility folder for rootDirectory path module, utilizes require.main.filename

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("path");
+
+const rootDir = require("../utilities/path");
+
 const router = express.Router();
 
 router.get("/add-product", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res) => {
